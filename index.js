3217526483ebc5d858c47ccd07e1268377892a1d
chiranth2428 <chiranth@interactivedigitalmedia.in>
2025-06-26 19:55:49 +0530

    user can now edit task on by clicking the task-container

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 let userTasks = [];
+let currentEditingTaskId = null;
+let numberOftasksLeft = userTasks.filter((task) => !task.isCompleted);
 
 const form = document.querySelector(".input-container");
 const titleInput = document.querySelector(".task-title");
 const container = document.querySelector(".tasks-list");
+const remainingTodos = document.querySelector(".todo-number");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -18,6 +21,7 @@ form.addEventListener("submit", function (event) {
 
   console.log(userTasks);
   renderTasks();
+  titleInput.value = "";
 });
 
 function renderTasks() {
@@ -32,7 +36,14 @@ function renderTasks() {
           }" class="isComplete-checkbox"  type="checkbox" ${
       task.isCompleted ? "checked" : ""
     }>
-          <p class = ${task.isCompleted ? "completed" : ""}>${task.title}</p>
+  ${
+    task.id === currentEditingTaskId
+      ? `<input data-task-id="${task.id}" class="edit-input" value="${task.title}">
+   <button data-task-id="${task.id}" class="save-btn">✓</button>`
+      : `<p data-task-id="${task.id}" class="task-title ${
+          task.isCompleted ? "completed" : ""
+        }">${task.title}</p>`
+  }
         </div>
           <button data-task-id="${task.id}" class="delete-btn">
            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
@@ -42,6 +53,7 @@ function renderTasks() {
 
     container.innerHTML += taskHTML;
   });
+  updateRemainingTodos();
 }
 
 // event delegation
@@ -54,6 +66,20 @@ container.addEventListener("click", function (event) {
     foundTask.isCompleted = !foundTask.isCompleted;
     console.log("found task objct", foundTask);
     renderTasks();
+  } else if (event.target.classList.contains("task-title")) {
+    const taskId = event.target.dataset.taskId;
+    currentEditingTaskId = taskId;
+    renderTasks();
+  } else if (event.target.classList.contains("save-btn")) {
+    const taskId = event.target.dataset.taskId;
+    const inputElement =
+      event.target.parentElement.querySelector(".edit-input");
+    const newTitle = inputElement.value;
+    const foundTask = userTasks.find((task) => task.id === taskId);
+    foundTask.title = newTitle;
+
+    currentEditingTaskId = null;
+    renderTasks();
   } else if (
     event.target.classList.contains("delete-btn") ||
     event.target.closest(".delete-btn")
@@ -65,3 +91,22 @@ container.addEventListener("click", function (event) {
     renderTasks();
   }
 });
+
+container.addEventListener("keypress", function (event) {
+  if (event.target.classList.contains("edit-input") && event.key === "Enter") {
+    const taskId = event.target.dataset.taskId;
+    const newTitle = event.target.value;
+
+    const foundTask = userTasks.find((task) => task.id === taskId);
+    foundTask.title = newTitle;
+
+    currentEditingTaskId = null;
+    renderTasks();
+  }
+});
+
+function updateRemainingTodos() {
+  const taskLeftCount = userTasks.filter((task) => !task.isCompleted).length;
+  remainingTodos.textContent = taskLeftCount;
+}
+function renderFooter() {}
